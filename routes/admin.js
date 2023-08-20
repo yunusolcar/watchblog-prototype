45f8db100fbce383c469e9f343b17e87bed67cc5
Yunus <yunusolcar@hotmail.com>
2023-08-20 11:52:57 +0300

    blog delete structure added

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,45 @@ const express = require("express")
 const db = require('../models/db')
 const router = express.Router()
 
+router.get("/blog/delete/:blogid", async (req, res) => {
+    const blogid = req.params.blogid // Parametreden alınan blogid
+
+    try {
+        const [blogs, ] = await db.execute("SELECT * FROM blog WHERE blogid=?", [blogid])
+        const blog = blogs[0]
+
+        res.render("admin/blog-delete", {
+            title: "Delete Blog",
+            blog: blog
+        })
+
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+router.post("/blog/delete/:blogid", async (req, res) => {
+    const blogid = req.body.blogid // Formun içerisinden alınan blogid
+
+    try {
+        await db.execute("DELETE  FROM blog WHERE blogid=?", [blogid])
+        res.redirect("/admin/blogs")
+
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+//Blog Create
 router.get("/blog/create", async (req, res) => {
+
     try {
         const [categories, ] = await db.execute("SELECT * FROM category")
         res.render("admin/blog-create", {
             title: "Add Blog",
             categories: categories
         })
+
     } catch (err) {
         console.log(err)
     }
@@ -30,7 +62,7 @@ router.post("/blog/create", async (req, res) => {
 })
 
 router.get("/blogs/:blogid", async (req, res) => {
-    const blogid = req.params.blogid //"/blogs/:blogid buradan blogid gelir
+    const blogid = req.params.blogid //"router.get("/blogs/:blogid") buradan blogid gelir
 
     try {
         const [blogs, ] = await db.execute("SELECT * FROM blog WHERE blogid=?", [blogid])
@@ -38,13 +70,30 @@ router.get("/blogs/:blogid", async (req, res) => {
         const blog = blogs[0]
 
         if (blog) {
-            return res.render("admin/blog-edit", {
+            return res.render("admin/blog-edit", { //return edilerek aşağıdaki kodların çalışması engellenir
                 title: blog.title,
                 blog: blog,
                 categories: categories
             });
         }
         res.redirect("/admin/blog")
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+router.post("/blogs/:blogid", async (req, res) => {
+
+    const blogid = req.body.blogid
+    const title = req.body.title
+    const description = req.body.description
+    const image = req.body.image
+    const categoryid = req.body.category
+
+    try {
+        await db.execute("UPDATE blog SET title=?, description=?, image=?, categoryid=? WHERE blogid=?", [title, description, image, categoryid, blogid])
+        res.redirect("/admin/blogs")
+
     } catch (err) {
         console.log(err)
     }
