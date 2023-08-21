d9434bcb44d9388daa274abc6438767b1ff4cb92
Yunus <yunusolcar@hotmail.com>
2023-08-21 12:32:38 +0300

    category CRUD operations added

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const express = require("express")
 const db = require('../models/db')
 const router = express.Router()
 
+//Delete Blog - get
 router.get("/blog/delete/:blogid", async (req, res) => {
     const blogid = req.params.blogid // Parametreden alınan blogid
 
@@ -18,20 +19,48 @@ router.get("/blog/delete/:blogid", async (req, res) => {
         console.log(err)
     }
 })
-
+//Delete Blog - post
 router.post("/blog/delete/:blogid", async (req, res) => {
     const blogid = req.body.blogid // Formun içerisinden alınan blogid
 
     try {
         await db.execute("DELETE  FROM blog WHERE blogid=?", [blogid])
-        res.redirect("/admin/blogs")
+        res.redirect("/admin/blogs?action=delete")
 
     } catch (err) {
         console.log(err)
     }
 })
+//Delete Category - get
+router.get("/category/delete/:categoryid", async (req, res) => {
+    const categoryid = req.params.categoryid // Parametreden alınan blogid
 
-//Blog Create
+    try {
+        const [categories, ] = await db.execute("SELECT * FROM category WHERE categoryid=?", [categoryid])
+        const category = categories[0]
+
+        res.render("admin/category-delete", {
+            title: "Delete Category",
+            category: category
+        })
+
+    } catch (err) {
+        console.log(err)
+    }
+})
+//Delete Category - post
+router.post("/category/delete/:categoryid", async (req, res) => {
+    const categoryid = req.body.categoryid // Formun içerisinden alınan blogid
+
+    try {
+        await db.execute("DELETE  FROM category WHERE categoryid=?", [categoryid])
+        res.redirect("/admin/categories?action=delete")
+
+    } catch (err) {
+        console.log(err)
+    }
+})
+//Crate Blog - get
 router.get("/blog/create", async (req, res) => {
 
     try {
@@ -45,7 +74,7 @@ router.get("/blog/create", async (req, res) => {
         console.log(err)
     }
 })
-
+//Create Blog - post
 router.post("/blog/create", async (req, res) => {
     const title = req.body.title // req.body.title => blog-create deki name alanından geliyor.
     const description = req.body.description
@@ -55,12 +84,35 @@ router.post("/blog/create", async (req, res) => {
 
     try {
         await db.execute("INSERT INTO blog(title, description, image, categoryid) VALUES (?,?,?,?)", [title, description, image, category])
-        res.redirect("/admin/blogs")
+        res.redirect("/admin/blogs?action=create")
     } catch (err) {
         console.log(err)
     }
 })
+//Create Category - get
+router.get("/category/create", async (req, res) => {
 
+    try {
+        res.render("admin/category-create", {
+            title: "Add Category"
+        })
+
+    } catch (err) {
+        console.log(err)
+    }
+})
+//Create Category - post
+router.post("/category/create", async (req, res) => {
+    const name = req.body.name
+
+    try {
+        await db.execute("INSERT INTO category(name) VALUES (?)", [name])
+        res.redirect("/admin/categories?action=create")
+    } catch (err) {
+        console.log(err)
+    }
+})
+//Edit Blog - get
 router.get("/blogs/:blogid", async (req, res) => {
     const blogid = req.params.blogid //"router.get("/blogs/:blogid") buradan blogid gelir
 
@@ -81,7 +133,7 @@ router.get("/blogs/:blogid", async (req, res) => {
         console.log(err)
     }
 })
-
+//Edit Blog - post
 router.post("/blogs/:blogid", async (req, res) => {
 
     const blogid = req.body.blogid
@@ -91,21 +143,70 @@ router.post("/blogs/:blogid", async (req, res) => {
     const categoryid = req.body.category
 
     try {
-        await db.execute("UPDATE blog SET title=?, description=?, image=?, categoryid=? WHERE blogid=?", [title, description, image, categoryid, blogid])
-        res.redirect("/admin/blogs")
+        await db.execute("UPDATE blog SET title=?, description=?, image=?, categoryid=? WHERE blogid=?", [title, description, image, categoryid, blogid]) // [title, description, image, categoryid, blogid] burada body den almış olduğu blogid yi alıp ona göre hangi id yi update edeceğimizi belirtiyoruz
+        res.redirect("/admin/blogs?action=edit&blogid=" + blogid)
 
     } catch (err) {
         console.log(err)
     }
 
 })
+//Edit Category - get
+router.get("/categories/:categoryid", async (req, res) => {
+    const categoryid = req.params.categoryid //"router.get("/categories/:categoryid") buradan categoryid gelir
 
+    try {
+        const [categories, ] = await db.execute("SELECT * FROM category WHERE categoryid=?", [categoryid])
+        const category = categories[0]
+
+        if (category) {
+            return res.render("admin/category-edit", { //return edilerek aşağıdaki kodların çalışması engellenir
+                title: category.name,
+                category: category,
+            });
+        }
+        res.redirect("/admin/categories")
+    } catch (err) {
+        console.log(err)
+    }
+})
+//Edit Category - post
+router.post("/categories/:categoryid", async (req, res) => {
+
+    const categoryid = req.body.categoryid
+    const name = req.body.name
+
+    try {
+        await db.execute("UPDATE category SET name=? WHERE categoryid=?", [name, categoryid]) // [name, categoryid] burada body den almış olduğu catid yi alıp ona göre hangi id ye göre  update yapılacağını belirtiyoruz
+        res.redirect("/admin/categories?action=edit&blogid=" + categoryid)
+
+    } catch (err) {
+        console.log(err)
+    }
+})
+// Blog List - get
 router.get("/blogs", async (req, res) => {
     try {
         const [blogs, ] = await db.execute("SELECT blogid, title, image FROM blog")
         res.render("admin/blog-list", {
             title: "Blog List",
-            blogs: blogs
+            blogs: blogs,
+            action: req.query.action, //Query içerisinde oluşturulmuş olan key bilgisi actiondan gelir 
+            blogid: req.query.blogid
+        });
+    } catch (err) {
+        console.log(err)
+    }
+})
+// Blog List - post
+router.get("/categories", async (req, res) => {
+    try {
+        const [categories, ] = await db.execute("SELECT * FROM category")
+        res.render("admin/category-list", {
+            title: "Category List",
+            categories: categories,
+            action: req.query.action, //Query içerisinde oluşturulmuş olan key bilgisi actiondan gelir 
+            categoryid: req.query.categoryid
         });
     } catch (err) {
         console.log(err)
