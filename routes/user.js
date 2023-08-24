03ef48acdd61ca0c6a29b63e9c07a6c2ad708ac1
Yunus <yunusolcar@hotmail.com>
2023-08-24 10:39:19 +0300

    sequelize - filter operations

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,21 @@
 const express = require("express");
-const db = require('../models/db')
-const router = express.Router();
+const Blog = require("../models/blog")
+const Category = require("../models/category")
 
+const router = express.Router();
 
 router.use("/blogs/category/:categoryid", async (req, res) => {
     const cat_id = req.params.categoryid
     try {
-        const [blogs, ] = await db.execute("SELECT * FROM blog WHERE categoryid=?", [cat_id])
-        const [categories, ] = await db.execute("SELECT * FROM category")
+        const blogs = await Blog.findAll({
+            where: {
+                categoryid: cat_id
+            },
+            raw: true
+        })
+        const categories = await Category.findAll({
+            raw: true
+        })
 
         res.render("users/blogs", {
             title: "Tüm Saatler", //statik veri
@@ -16,19 +24,15 @@ router.use("/blogs/category/:categoryid", async (req, res) => {
         })
 
     } catch (error) {
-
+        console.log(error);
     }
 })
-
-
 //Blogs/:id
-router.use("/blogs/:blogid", async (req, res) => { // /blogs/? soru işareti yerine sayı geldiğinde (yani blogid (blogid yerine başka şey de yazabiliriz)) bunu alttaki params içerisinden alabiliriz
+router.use("/blogs/:blogid", async (req, res) => {
     const id = req.params.blogid
-    //console.log("id = " + id) // Burada url e yazılan id bilgisi konsola yazıldı. buraya girilen id deki veriler alınır. bütün kayıtlar alınmaz
     try {
-        const [blogs, ] = await db.execute("SELECT * FROM blog WHERE blogid=?", [id]) // Soru işareti ile koyduktan sonra execute() 'a ikinci bir parametreyi Dizi olarak ekliyoruz. iki parametre varsa iki değer atanır
+        const blog = await Blog.findByPk(id)
 
-        const blog = blogs[0];
         if (blog) {
             return res.render("users/blog-details", { // ilgili id si olan obje database de varsa  return dönüp aşağıdaki işlemleri yapar yok ise anasayfaya redirect eder
                 title: blog.title, //diinamik veri
@@ -41,14 +45,17 @@ router.use("/blogs/:blogid", async (req, res) => { // /blogs/? soru işareti yer
         console.log(error)
     }
 });
-
 //Blogs
 router.use("/blogs", async (req, res) => {
     try {
-        const [blogs, ] = await db.execute("SELECT * FROM blog") // [blogs, ] = blogs değişkenine, dizinin ilk elemanı atanır. 
-        const [categories, ] = await db.execute("SELECT * FROM category") // [category, ] = category değişkenine, dizinin ilk elemanı atanır.
-        // console.log(blogs[2].title) 2. indisteki kaydın title bilgisini konsola yazdırır
-        res.render("users/index", {
+        const blogs = await Blog.findAll({
+            raw: true
+        })
+        const categories = await Category.findAll({
+            raw: true
+        })
+
+        res.render("users/blogs", {
             title: "Tüm Saatler",
             blogs: blogs,
             categories: categories
@@ -57,12 +64,15 @@ router.use("/blogs", async (req, res) => {
         console.log(err)
     }
 })
-
 //Index
 router.use("/", async (req, res) => {
     try {
-        const [blogs, ] = await db.execute("SELECT * FROM blog") // [blogs, ] = blogs değişkenine, dizinin ilk elemanı atanır. 
-        const [categories, ] = await db.execute("SELECT * FROM category") // [category, ] = category değişkenine, dizinin ilk elemanı atanır.
+        const blogs = await Blog.findAll({
+            raw: true
+        })
+        const categories = await Category.findAll({
+            raw: true
+        })
         // console.log(blogs[2].title) 2. indisteki kaydın title bilgisini konsola yazdırır
         res.render("users/index", {
             title: "Popüler Saatler", //statik veri
@@ -73,6 +83,5 @@ router.use("/", async (req, res) => {
         console.log(err)
     }
 })
-
 
 module.exports = router;
