6c170688e5694cc39512fc10eb5be03925da4d60
yunus <yunusolcar@hotmail.com>
2023-09-27 21:35:21 +0300

    route protection

--- routes/admin.js
+++ routes/admin.js
@@ -1,48 +1,49 @@
 const express = require("express");
 const adminController = require("../controllers/adminController");
 const imageUpload = require("../helpers/image-upload");
+const isAuth = require("../middlewares/auth");
 const router = express.Router();
 
 //Delete Blog - get
-router.get("/blog/delete/:blogid", adminController.getDeleteBlog);
+router.get("/blog/delete/:blogid", isAuth, adminController.getDeleteBlog);
 
 //Delete Blog - post
-router.post("/blog/delete/:blogid", adminController.postDeleteBlog);
+router.post("/blog/delete/:blogid", isAuth, adminController.postDeleteBlog);
 
 //Delete Category - get
-router.get("/category/delete/:categoryid", adminController.getCategoryDelete);
+router.get("/category/delete/:categoryid", isAuth, adminController.getCategoryDelete);
 
 //Delete Category - post
-router.post("/category/delete/:categoryid", adminController.postCategoryDelete);
+router.post("/category/delete/:categoryid", isAuth, adminController.postCategoryDelete);
 
 //Create Blog - get
-router.get("/blog/create", adminController.getBlogCreate);
+router.get("/blog/create", isAuth, adminController.getBlogCreate);
 
 //Create Blog - post
-router.post("/blog/create", imageUpload.upload.single("image"), adminController.postBlogCreate);
+router.post("/blog/create", isAuth, imageUpload.upload.single("image"), adminController.postBlogCreate);
 
 //Create Category - get
-router.get("/category/create", adminController.getCategoryCreate);
+router.get("/category/create", isAuth, adminController.getCategoryCreate);
 
 //Create Category - post
-router.post("/category/create", adminController.postCategoryCreate);
+router.post("/category/create", isAuth, adminController.postCategoryCreate);
 
 //Edit Blog - get
-router.get("/blogs/:blogid", adminController.getBlogEdit);
+router.get("/blogs/:blogid", isAuth, adminController.getBlogEdit);
 
 //Edit Blog - post
-router.post("/blogs/:blogid", imageUpload.upload.single("image"), adminController.postBlogEdit);
+router.post("/blogs/:blogid", isAuth, imageUpload.upload.single("image"), adminController.postBlogEdit);
 
 //Edit Category - get
-router.get("/categories/:categoryid", adminController.getCategoryEdit);
+router.get("/categories/:categoryid", isAuth, adminController.getCategoryEdit);
 
 //Edit Category - post
-router.post("/categories/:categoryid", adminController.postCategoryEdit);
+router.post("/categories/:categoryid", isAuth, adminController.postCategoryEdit);
 
 // Blog List - get
-router.get("/blogs", adminController.getBlogList);
+router.get("/blogs", isAuth, adminController.getBlogList);
 
 // Category List - post
-router.get("/categories", adminController.getCategoryList);
+router.get("/categories", isAuth, adminController.getCategoryList);
 
 module.exports = router;
